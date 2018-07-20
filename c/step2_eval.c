852187d0a50e3f836cf295d4b7f3418f761613a9
Moritz Schönherr <moritz.schoenherr@posteo.net>
2018-07-20 13:26:35 +0200

    org: setup for step2

---
+++ c/step2_eval.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "linenoise/linenoise.h"
+
+#include "types.h"
+#include "reader.h"
+#include "printer.h"
+#include "ll.c/ll.h"
+
+/* Transforms input string to an ast*/
+mal_v* READ(char* input) {
+
+  return read_str(input);
+}
+
+/* Stub function returning its arg */
+mal_v* EVAL(mal_v* ast) {
+
+  return ast;
+}
+
+/* Printing ast to a string*/
+char* PRINT(mal_v* ast) {
+
+  return pr_str(ast);
+}
+
+/* Calls READ, EVAL, PRINT in order and returns 
+   the result of PRINT */
+char* rep(char* input) {
+
+  return PRINT(EVAL(READ(input)));
+
+}
+
+/* Endlessly calls rep and prints its result */
+int main(int* argc, char** argv) {
+
+  char* input;
+  linenoiseHistorySetMaxLen(1000);
+
+  while(input = linenoise("mal-user> ")) {
+
+    linenoiseHistoryAdd(input);
+    char* to_print = rep(input);
+    puts(to_print);
+    free(to_print);
+    linenoiseFree(input);
+  }
+
+  return 0;
+}
